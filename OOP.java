b942e43f5455b6a918b6d1e9fe368521b693fff0
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-04 23:57:54 +0530

    Add Student class demonstrating encapsulation
    Implement Student class with encapsulation for name and age.

---
+++ OOP.java
@@ -0,0 +1,30 @@
+//The 4 Pillars of OOP--
+    //1.Encapsulation
+    //2.Abstraction
+    //3.Inheritance
+    //4.Polymorphism
+//==============================================================================================================
+
+//Encapsualtions
+class Student {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+//======================================================================================================
+
+
+
+
+
+
+
+}
