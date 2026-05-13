6f417ed928c23151bfe3a9a63f03137de4b89843
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-13 15:10:28 +0530

    Implement logic to find second largest element in array
    Added logic to find the second largest element after determining the largest element in the array.

--- array/Second_Largest.java
+++ array/Second_Largest.java
@@ -2,12 +2,14 @@
 class Main {
     public static void main(String[] args) {
         int arr[]={1,4,3,5,8,10};
+        //before finding Second largest element , we need to findout Largest element of that array.
         int largest=Integer.MIN_VALUE;
         for(int num:arr){
             if(num>largest){
                 largest=num;
             }
         }
+        //logic to findout second largest element
         int Second_largest=Integer.MIN_VALUE;
         for(int num:arr){
             if(num>Second_largest && num!=largest){
