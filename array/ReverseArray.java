efcff70d0ece09ff19aa003ad2f12c244e778322
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-14 14:22:22 +0530

    Add array reversal logic in ReverseArray.java
    Implemented a method to reverse an array in place and print the result.

--- array/ReverseArray.java
+++ array/ReverseArray.java
@@ -1,1 +1,21 @@
-
+class Main {
+    public static void main(String[] args) {
+      int arr[]={1,2,3,4};
+      int left=0;
+      int right=arr.length-1;
+      while(left<right){
+         int temp=arr[left];
+         arr[left]=arr[right];
+         arr[right]=temp;
+         left++;
+         right--;
+      }
+    //   printing result
+    for(int num:arr){
+      System.out.print(num+" ");
+    }
+    
+    
+      
+    }
+}
