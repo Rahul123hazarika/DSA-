8a03bcf633dddf9ce6e143991d6df5cd8ff5245b
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-17 11:17:35 +0530

    Add array rotation functionality in arrayRotation.java
    Implement array rotation method to rotate elements.

---
+++ array/arrayRotation.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class rotation{
+    static void ArrayRotation(int arr[], int d){
+
+        int n=arr.length;
+        //repeat array rotation d times
+        for(int i=0;i<d;i++){
+           int first=arr[0];
+           for(int j=0;j<n-1;j++){
+            arr[j]=arr[j+1];
+           }
+           arr[n-1]=first;
+        }
+    }
+    public static void  main(String args[]){
+        int arr[]={1,2,3,4,5};
+        int d=2;
+        ArrayRotation(arr,d);
+        System.out.println(Arrays.toString(arr));
+      
+        
+    }
+}
