0563ae149b7bcd8f690b7d8d0b259db247962890
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-26 12:09:24 +0530

    Add twoSum method to check for target sum
    Implement twoSum method to find if any two numbers in the array sum to the target.

--- Interview_Questions/Two Sum.java
+++ Interview_Questions/Two Sum.java
@@ -1,1 +1,28 @@
-
+class Main{
+    static boolean twoSum(int arr[], int target){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+          // For each element arr[i], check every
+          // other element arr[j] that comes after it
+            for(int j=i+1;j<n;j++){
+                // Check if the sum of the current pair, equals the target
+                if(arr[i]+arr[j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+    
+    public static void main(String args[]){
+        int arr[]={1,2,4,5,6};
+        int target=9;
+        if(twoSum(arr,target)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
