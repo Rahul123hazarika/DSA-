81c215947623b63c5ba5c78a68546b2a9ab7fd2f
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-06-23 11:18:39 +0530

    Add Java program to check divisibility by 6

---
+++ array/Basic Coding Problems/divisible_by_six.java
@@ -0,0 +1,15 @@
+// To check whether the given number is divisible by 6 or not
+import java.io.*;
+
+class Divisible_by_six{
+    public static void main (String[] args) {
+        long n = 36358839596L;
+ 
+        // finding given number is divisible by 6 or not
+        if (n % 6 == 0)
+              System.out.print("Yes");
+        else
+              System.out.print("No");
+    }
+}
+
