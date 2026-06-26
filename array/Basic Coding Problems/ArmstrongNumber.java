053abc1ccbad88a0ebb6954be10d1325f92ccff3
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-06-26 12:33:07 +0530

    Add ArmstrongNumber class to check Armstrong numbers

---
+++ array/Basic Coding Problems/ArmstrongNumber.java
@@ -0,0 +1,29 @@
+// check the number is armstrong number or not ?
+import java.util.Scanner;
+class ArmstrongNumber{
+    public static boolean isArmstrong(int num){
+        int originalNumber=num;
+        int sum=0;
+        int digit=String.valueOf(num).length();
+        while(num!=0){
+            int x=num%10;
+            sum +=Math.pow(x, digit);
+            num=num/10;
+        }
+        return sum==originalNumber;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int num=sc.nextInt();
+        if(isArmstrong(num)){
+            System.out.println("the number is an armstrong number");
+        }
+        else{
+            System.out.println("number is not an armstrong number");
+        }
+        sc.close();
+    }
+    
+}
